cb5927e4ab648772ac03a4105ddefb211467bae1
Rayanne Nóbrega <nanyz.nobrega@gmail.com>
2022-05-01 21:26:54 -0300

    invoke command

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -62,7 +62,7 @@ describe('Our first suite', () =>{
 
     })
 
-    it.only('then and wrap method', () =>{
+    it('then and wrap method', () =>{
 
         cy.visit('/')
         cy.contains('Forms').click()
@@ -98,6 +98,50 @@ describe('Our first suite', () =>{
                 cy.wrap(secondForm).find('[for="exampleInputPassword1"]').should('contain','Password')
             })
 
+        })
+
+    })
+
+    it('Invoke command', () =>{
+        
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        //1
+        cy.get('[for="exampleInputEmail1"]').should('contain', 'Email address')
+
+        //2
+        cy.get('[for="exampleInputEmail1"]').then(label =>{
+            expect(label.text()).to.equal('Email address')
+        })
+
+        //3
+        cy.get('[for="exampleInputEmail1"]').invoke('text').then(text =>{
+            expect(text).to.equal('Email address')
+        })
+
+        cy.contains('nb-card', 'Basic form')
+            .find('nb-checkbox')
+            .click()
+            .find('.custom-checkbox')
+            .invoke('attr','class')
+            //.should('contain', 'checked')
+            .then(classValue =>{
+                expect(classValue).to.contain('checked')
+            })
+
+    })
+
+    it.only('assert propert', () =>{
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click()
+
+        cy.contains('nb-card', 'Common Datepicker').find('input').then(input =>{
+            cy.wrap(input).click()
+            cy.get('nb-calendar-day-picker').contains('17').click()
+            cy.wrap(input).invoke('prop', 'value').should('contain','May 17, 2022')
         })
 
     })
