7d9e28c7b0fd6f764d4407e3895ab3bd8b3ff4b6
Rayanne Nóbrega <nanyz.nobrega@gmail.com>
2022-05-04 10:05:41 -0300

    checkboxes and radio buttons

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -133,7 +133,7 @@ describe('Our first suite', () =>{
 
     })
 
-    it.only('assert propert', () =>{
+    it('assert propert', () =>{
         cy.visit('/')
         cy.contains('Forms').click()
         cy.contains('Datepicker').click()
@@ -143,7 +143,42 @@ describe('Our first suite', () =>{
             cy.get('nb-calendar-day-picker').contains('17').click()
             cy.wrap(input).invoke('prop', 'value').should('contain','May 17, 2022')
         })
+    })
+
+    it('radio button', () =>{
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        cy.contains('nb-card','Using the Grid').find('[type="radio"]').then(radioButtons =>{
+            cy.wrap(radioButtons)
+                .first()
+                .check({force:true})
+                .should('be.checked')
+
+            cy.wrap(radioButtons)
+                .eq(1)
+                .check({force:true})
+
+            cy.wrap(radioButtons)
+                .first()
+                .should('not.be.checked')
+
+            cy.wrap(radioButtons)
+                .eq(2)
+                .should('be.disabled')
+        })
+    })
+
+    it.only('check boxes', () =>{
+        cy.visit('/')
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Toastr').click()
+
+        cy.get('[type="checkbox"]').check({force:true})
+        cy.get('[type="checkbox"]').eq(0).click({force:true})
 
     })
+
 
 })
